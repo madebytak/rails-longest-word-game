26d99b39d3953b38c0d172a100054a647297f640
Takeshi Shinohara <takeshigs@gmail.com>
2022-02-14 16:30:12 +0900

    working on the logic of the answer

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,8 +9,19 @@ class GamesController < ApplicationController
   def score
     @answer = params[:word]
     @choice_of_letters = params[:choice_of_letters]
-    if @choice_of_letters
+    # break the answer to letters to check if part of the choices
+    @answer.upcase.split('').each do |letter|
+    if @choice_of_letters.include?(letter) &&
+    end
+  end
 
+  def english_check(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{answer}"
+    word_check = URI.open(url).read
+    word = JSON.parse(word_check)
+    word['found']
+  end
+end
 
 # Sorry but #{@answer} can't be built out of the letter
 # Sorry but #{@answer} does not seem to be a valid English word
@@ -18,12 +29,3 @@ class GamesController < ApplicationController
 # need to check if the answer is a valid english word. Set as method to call it?
 # pass the english_word check and give it to the score for tesing
 # validate by checking if word is english is included in the choices of letters
-  end
-
-  def english_word(word)
-    url = "https://wagon-dictionary.herokuapp.com/#{answer}"
-    word_check = URI.open(url).read
-    word = JSON.parse(word_check)
-    word['found']
-  end
-end
